e0ffbfa37a93cdb87149f9e31bccec4d6075fd78
AnubhavSaxena43 <anubhavsa@zaamo.co>
2022-12-09 22:40:22 +0530

    feat:Chart Skeletal

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active Users": 4350,
+  },
+  {
+    name: "Feb",
+    "Active Users": 4900,
+  },
+  {
+    name: "Mar",
+    "Active Users": 4700,
+  },
+  {
+    name: "Apr",
+    "Active Users": 4100,
+  },
+  {
+    name: "May",
+    "Active Users": 4340,
+  },
+  {
+    name: "Jun",
+    "Active Users": 4000,
+  },
+  {
+    name: "Jul",
+    "Active Users": 4090,
+  },
+  {
+    name: "Aug",
+    "Active Users": 3990,
+  },
+  {
+    name: "Sep",
+    "Active Users": 4030,
+  },
+  {
+    name: "Oct",
+    "Active Users": 4340,
+  },
+  {
+    name: "Nov",
+    "Active Users": 2000,
+  },
+  {
+    name: "Dec",
+    "Active Users": 3000,
+  },
+];
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Chart from "../../components/Chart/Chart";
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import { userData } from "../../dummyData";
 import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        grid={true}
+        title={"User Analytics"}
+        dataKey={"Active Users"}
+      />
     </div>
   );
 };
